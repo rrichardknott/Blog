7003e6210181b1228d389cececb7aa12d3e77e66
rrichardknott <67433117+rrichardknott@users.noreply.github.com>
2020-09-22 22:30:35 -0700

    Update Blog

--- App_Start/IdentityConfig.cs
+++ App_Start/IdentityConfig.cs
@@ -20,6 +20,7 @@ namespace Blog
       public class EmailService : IIdentityMessageService
       {
             public Task SendAsync(IdentityMessage message)
+
             {
                   // Plug in your email service here to send an email.
                   return Task.FromResult(0);
